e1c9eb7734ddc5639c3726252dac3384f954a440
Daniel Ramos <danielramosacosta1@gmail.com>
2024-04-09 21:22:11 +0200

    add storybook

---
+++ src/components/Button.stories.ts
@@ -0,0 +1,29 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { fn } from "@storybook/test";
+import { Button } from "./Button";
+
+const meta = {
+  title: "Nextjs/Button",
+  component: Button,
+  tags: ["autodocs"],
+  argTypes: {},
+  args: { onClick: fn() },
+} satisfies Meta<typeof Button>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+// More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
+export const Primary: Story = {
+  args: {
+    title: "Primary",
+  },
+};
+
+export const Secondary: Story = {
+  args: {
+    title: "Secondary",
+    type: "secondary",
+  },
+};
