57b207512a79032f320bb7288a2265ab15ea1305
Daniel Ramos <danielramosacosta1@gmail.com>
2024-04-10 19:06:52 +0200

    add dark/bright theme

--- src/components/Button.stories.ts
+++ src/components/Button.stories.ts
@@ -14,7 +14,6 @@ export default meta;
 
 type Story = StoryObj<typeof meta>;
 
-// More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
 export const Primary: Story = {
   args: {
     title: "Primary",
